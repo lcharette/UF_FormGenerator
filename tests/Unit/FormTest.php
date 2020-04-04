bc82eab2fbafaf997ecb32df970123b689ffcf3f
Louis Charette <charette.louis@gmail.com>
2020-04-03 20:02:12 -0400

    Move tests to dataprovider

--- tests/Unit/FormTest.php
+++ tests/Unit/FormTest.php
@@ -32,56 +32,17 @@ class FormTest extends TestCase
 
     /**
      * Test the Form Class.
-     * Run the test with no current values (empty form).
+     *
+     * @param string $file
+     * @param array $data
+     * @param array $expected
+     * @dataProvider formProvider
      */
-    public function testForm(): void
+    public function testForm(string $file, array $data, array $expected): void
     {
         // Get Schema
-        $loader = new YamlFileLoader($this->basePath . '/good.json');
+        $loader = new YamlFileLoader($this->basePath . $file);
         $schema = new RequestSchemaRepository($loader->load());
-
-        // Generate the form
-        $form = new Form($schema);
-
-        // Test to make sure the class creation is fine
-        $this->assertInstanceof(Form::class, $form);
-
-        // Test the form generation
-        $generatedForm = $form->generate();
-        $this->assertIsArray($generatedForm);
-
-        // Test one of the form input
-        $expected = [
-            'name' => [
-                'autocomplete' => 'off',
-                'class'        => 'form-control',
-                'value'        => '',
-                'name'         => 'name',
-                'id'           => 'field_name',
-                'type'         => 'text',
-                'label'        => 'Project Name',
-                'icon'         => 'fa-flag',
-                'placeholder'  => 'Project Name',
-            ],
-        ];
-
-        // We test the generated result
-        $this->assertSame($expected, $generatedForm);
-    }
-
-    /**
-     * Test the Form Clas with values to make sure filled form works correctly.
-     */
-    public function testFormWithData(): void
-    {
-        // Get Schema
-        $loader = new YamlFileLoader($this->basePath.'/good.json');
-        $schema = new RequestSchemaRepository($loader->load());
-
-        // The data
-        $data = [
-            'name' => 'Bar project',
-        ];
 
         // Generate the form
         $form = new Form($schema, $data);
@@ -93,66 +54,77 @@ class FormTest extends TestCase
         $generatedForm = $form->generate();
         $this->assertIsArray($generatedForm);
 
-        // Test one of the form input
-        $expected = [
-            'name' => [
-                'autocomplete' => 'off',
-                'class'        => 'form-control',
-                'value'        => 'Bar project', //Value's here !
-                'name'         => 'name',
-                'id'           => 'field_name',
-                'type'         => 'text',
-                'label'        => 'Project Name',
-                'icon'         => 'fa-flag',
-                'placeholder'  => 'Project Name',
-            ]
-        ];
-
         // We test the generated result
-        $this->assertEquals($expected, $generatedForm);
+        $this->assertSame($expected, $generatedForm);
     }
 
     /**
-     * Test a non existant input type. It's supposed to not find the class and
-     * default back to the `Text` element class.
+     * Data provider for tests
      */
-    public function testUndefinedFormElement(): void
+    public function formProvider(): array
     {
-        // Get Schema
-        $loader = new YamlFileLoader($this->basePath . '/bad.json');
-        $schema = new RequestSchemaRepository($loader->load());
-
-        // Generate the form
-        $form = new Form($schema);
-
-        // Test to make sure the class creation is fine
-        $this->assertInstanceof(Form::class, $form);
-
-        // Test the form generation
-        $generatedForm = $form->generate();
-        $this->assertIsArray($generatedForm);
-
-        // Test one of the form input
-        $expected = [
-            'myField' => [
-                'autocomplete' => 'off',
-                'class'        => 'form-control',
-                'value'        => '',
-                'name'         => 'myField',
-                'id'           => 'field_myField',
-                'type'         => 'foo', // Will still be foo, but the whole element is parsed using the text parser
+        return [
+            // WITH NO DATA
+            [
+                '/good.json',
+                [],
+                [
+                    'name' => [
+                        'autocomplete' => 'off',
+                        'class'        => 'form-control',
+                        'value'        => '',
+                        'name'         => 'name',
+                        'id'           => 'field_name',
+                        'type'         => 'text',
+                        'label'        => 'Project Name',
+                        'icon'         => 'fa-flag',
+                        'placeholder'  => 'Project Name',
+                    ],
+                ]
             ],
-            'myOtherField' => [
-                'autocomplete' => 'off',
-                'class'        => 'form-control',
-                'value'        => 'Bar',
-                'name'         => 'myOtherField',
-                'id'           => 'field_myOtherField',
-                'type'         => 'text', // Will be added by the FORM class
+            // WITH DATA
+            [
+                '/good.json',
+                [
+                    'name' => 'Bar project',
+                ],
+                [
+                    'name' => [
+                        'autocomplete' => 'off',
+                        'class'        => 'form-control',
+                        'value'        => 'Bar project', //Value's here !
+                        'name'         => 'name',
+                        'id'           => 'field_name',
+                        'type'         => 'text',
+                        'label'        => 'Project Name',
+                        'icon'         => 'fa-flag',
+                        'placeholder'  => 'Project Name',
+                    ],
+                ]
+            ],
+            // WITH BAD DATA
+            [
+                '/bad.json',
+                [],
+                [
+                    'myField' => [
+                        'autocomplete' => 'off',
+                        'class'        => 'form-control',
+                        'value'        => '',
+                        'name'         => 'myField',
+                        'id'           => 'field_myField',
+                        'type'         => 'foo', // Will still be foo, but the whole element is parsed using the text parser
+                    ],
+                    'myOtherField' => [
+                        'autocomplete' => 'off',
+                        'class'        => 'form-control',
+                        'value'        => 'Bar',
+                        'name'         => 'myOtherField',
+                        'id'           => 'field_myOtherField',
+                        'type'         => 'text', // Will be added by the FORM class
+                    ],
+                ]
             ],
         ];
-
-        // We test the generated result
-        $this->assertSame($expected, $generatedForm);
     }
 }
